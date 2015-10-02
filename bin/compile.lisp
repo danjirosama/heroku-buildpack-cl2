0fd620a45b261030b344925e337562660cdc2932
Lucas <lucashpandolfo@gmail.com>
2015-10-02 19:51:01 -0300

    Remove the redefinition of print-backtrace for sbcl. Avoids package lock violation.

--- bin/compile.lisp
+++ bin/compile.lisp
@@ -26,14 +26,13 @@
 
 ;;; stacktrace printing, copy/pasted from the ql-test by Fare:
 ;;; ssh://common-lisp.net/home/frideau/git/ql-test.git
+#-sbcl
 (defun print-backtrace (out)
   "Print a backtrace (implementation-defined)"
   (declare (ignorable out))
   #+clozure (let ((*debug-io* out))
 	      (ccl:print-call-history :count 100 :start-frame-number 1)
-	      (finish-output out))
-  #+sbcl
-  (sb-debug:backtrace most-positive-fixnum out))
+	      (finish-output out)))
 
 (defun call-with-ql-test-context (thunk)
   (block nil
