f77139662ae996403706ccb9021379d33e81d69b
Lucas <lucashpandolfo@gmail.com>
2015-10-03 14:09:51 -0300

    Try another approach

--- bin/compile.lisp
+++ bin/compile.lisp
@@ -7,9 +7,6 @@
 (defvar *asdf-dir*  (pathname (concatenate 'string (uiop:getenv "CACHE_DIR") "/asdf/")))
 (defvar *buildpack-dir* (pathname (concatenate 'string (uiop:getenv "BUILDPACK_DIR") "/")))
 
-(defun heroku-setup-cache ()
-  (setf (uiop:getenv "XDG_CACHE_HOME") (format nil "~a" *asdf-dir*)))
-
 (defmacro fncall (funname &rest args)
   `(funcall (read-from-string ,funname) ,@args))
 
@@ -43,5 +40,4 @@
 
 ;;; Load the application compile script
 (with-ql-test-context ()
-  (heroku-setup-cache)
   (load (merge-pathnames "heroku-compile.lisp" *build-dir*)))
