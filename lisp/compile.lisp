5376a7e9712a36f362f2c75ea09ea37a1902decc
lucas <lpandolfo@citedef.gob.ar>
2015-10-05 12:01:53 -0300

    Reorganization and cleanups.

--- lisp/compile.lisp
+++ lisp/compile.lisp
@@ -2,29 +2,24 @@
 
 (require :asdf)
 
-(defvar *build-dir* (pathname (concatenate 'string (uiop:getenv "BUILD_DIR") "/")))
-(defvar *cache-dir* (pathname (concatenate 'string (uiop:getenv "CACHE_DIR") "/")))
+(defvar *build-dir*     (pathname (concatenate 'string (uiop:getenv "BUILD_DIR") "/")))
+(defvar *cache-dir*     (pathname (concatenate 'string (uiop:getenv "CACHE_DIR") "/")))
 (defvar *buildpack-dir* (pathname (concatenate 'string (uiop:getenv "BUILDPACK_DIR") "/")))
 
 (defmacro fncall (funname &rest args)
   `(funcall (read-from-string ,funname) ,@args))
 
-(defun require-quicklisp (&key version)
-  "VERSION if specified must be in format YYYY-MM-DD"
+(defun require-quicklisp ()
+  "Loads quicklisp."
   (let ((ql-setup (merge-pathnames "quicklisp/setup.lisp" *cache-dir*)))
     (if (probe-file ql-setup)
-        (progn (format t "Quicklisp already installed...~%Loading.....~%") 
+        (progn (format t "Quicklisp already installed...~%Loading.....~%")
                (load ql-setup))
         (progn
           (format t "Quicklisp not detected...~%Installing Quicklisp....~%")
-          (load (merge-pathnames "bin/quicklisp.lisp" *buildpack-dir*))
+          (load (merge-pathnames "quicklisp/quicklisp.lisp" *buildpack-dir*))
           (fncall "quicklisp-quickstart:install"
-                   :path (make-pathname :directory (pathname-directory ql-setup)))))
-    (when version
-      (fncall "ql-dist:install-dist"
-              (format nil "http://beta.quicklisp.org/dist/quicklisp/~A/distinfo.txt"
-                      version)
-              :replace t :prompt nil))))
+                   :path (make-pathname :directory (pathname-directory ql-setup)))))))
 
 (defun call-with-ql-test-context (thunk)
   (block nil
@@ -41,4 +36,7 @@
 
 ;;; Load the application compile script
 (with-ql-test-context ()
+  "Loads quicklisp and heroku-compile.lisp from the build directory."
+  (require-quicklisp) ;; Always require quicklisp
+  (asdf:disable-output-translations) ;; Put fasl's besides lisp files
   (load (merge-pathnames "heroku-compile.lisp" *build-dir*)))
